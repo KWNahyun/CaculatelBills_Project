d81d40d498e1f58de4c19fc474c2b3dad0b4b277
Jeonyuri <127704512+Jeonyuri@users.noreply.github.com>
2023-11-28 14:31:27 +0900

    Add files via upload

---
+++ tesseract/테서렉트.py
@@ -0,0 +1,20 @@
+from PIL import Image
+import numpy as np
+import pytesseract
+
+# 이미지 파일 경로
+filename = '/Users/jeon-yuli/Desktop/숭실대학교/1학년 2학기/프로그래밍2/프로젝트/스크린샷 2023-11-28 오후 1.55.47.png'
+
+# 이미지를 NumPy 배열로 변환
+img = np.array(Image.open(filename))
+
+# 이미지에서 텍스트 추출
+text = pytesseract.image_to_string(img, lang='eng+kor')
+
+# 추출된 텍스트를 txt 파일로 저장
+output_file_path = '/Users/jeon-yuli/Desktop/숭실대학교/1학년 2학기/프로그래밍2/프로젝트/extracted_text.txt'
+
+with open(output_file_path, 'w', encoding='utf-8') as file:
+    file.write(text)
+
+print(f'Text saved to {output_file_path}')
